0e75df9366fda22a084ad81f7d4a7b77db8d24f8
Jimmy Young <yangjufo@gmail.com>
2018-12-27 23:33:19 -0800

    change the position of search input

--- mysite/settings.py
+++ mysite/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '28yoma5epbzt^4iwz7sbarp337*^p5v-^p8t2b&s!a50$y26!$'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['127.0.0.1', 'localhost', '.jianyang995.com']
 
