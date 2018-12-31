b3899bd4933220caa8a71a6586e026f090b1d2a2
Jimmy Young <yangjufo@gmail.com>
2018-12-31 00:20:33 -0800

    add link in project detail view

--- mysite/views.py
+++ mysite/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 
 
 def index(request):
-    return render(request, 'basic/index.html', context={})
+    return render(request, 'myprojects/index.html', context={})
 
 
 def about(request):
