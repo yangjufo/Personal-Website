e54a8e9c10f42af1e9e179a600670dc68470d107
Jimmy Young <yangjufo@gmail.com>
2023-01-16 17:36:09 -0800

    No cache while install python dependencies.

--- deploy.sh
+++ deploy.sh
@@ -5,9 +5,9 @@ source_folder=/home/ubuntu/sites/www.jianyang995.com
 cd $source_folder/Personal-Website
 git reset *
 git pull
-sed -i 's/DEBUG = True/DEBUG = False/' ./mysite/settings.py 
+sed -i 's/DEBUG = True/DEBUG = False/' ./mysite/settings.py
 
-$source_folder/env/bin/pip install -r requirements.txt
+$source_folder/env/bin/pip install -r requirements.txt --no-cache-dir
 $source_folder/env/bin/python manage.py collectstatic --noinput
 $source_folder/env/bin/python manage.py migrate
 
