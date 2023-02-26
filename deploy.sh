5e24a27bd983570c68d5dce16b2feae7f0da9e71
Jimmy Young <yangjufo@gmail.com>
2023-02-26 15:53:30 -0800

    Update deploy.sh.

--- deploy.sh
+++ deploy.sh
@@ -5,7 +5,6 @@ source_folder=/home/ubuntu/sites/www.jianyang995.com
 cd $source_folder/Personal-Website
 git reset *
 git pull
-sed -i 's/DEBUG = True/DEBUG = False/' ./mysite/settings.py
 
 $source_folder/env/bin/pip install -r requirements.txt --no-cache-dir
 $source_folder/env/bin/python manage.py collectstatic --noinput
